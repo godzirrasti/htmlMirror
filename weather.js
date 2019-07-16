cb3058043d6f99cc862c1c8c313c264c4f69b656
Kevin Halliwell <kevinhalliwell@me.com>
2019-07-16 16:13:46 -0400

    Updates

--- weather.js
+++ weather.js
@@ -1,3 +1,29 @@
-function print(){
-		console.log(config);
+function weather() {
+	var location = document.getElementById("location");
+  	var apiKey = config;
+  	var url = "https://api.darksky.net/forecast/";
+
+  	navigator.geolocation.getCurrentPosition(success, error);
+
+  	function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    location.innerHTML =
+      	"Latitude is " + latitude + "° Longitude is " + longitude + "°";
+
+    $.getJSON(
+      	url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
+      	function(data) {
+        	$("#temp").html(data.currently.temperature + "° F");
+       	 	$("#minutely").html(data.minutely.summary);
+      	}
+    );
+  	}
+
+  	function error() {
+    	location.innerHTML = "Unable to retrieve your location";
+  	}
+
+  	location.innerHTML = "Locating...";
 }
