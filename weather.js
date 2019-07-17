e48c2c969623753da86fcc58874e320571748dc3
Kevin Halliwell <kevinhalliwell@me.com>
2019-07-17 16:32:28 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,7 +4,15 @@ function weather(){
 	var apiKey = config;
 	var url = "https://api.darksky.net/forecast/";
 	var timezone = "America/New York";
-	//var urlRequest = url + apiKey + String(geoLat)
+
+	//Check if the geolocation API exists
+	if (navigator.geolocation) {
+  		//true
+ 		 location.innerHTML = "Location found!!!";
+	} else {
+  		//false
+  		location.innerHTML = "Geolocation not available";
+	}
 }
 
 
