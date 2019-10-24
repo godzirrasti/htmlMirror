b788661d33199c9ed651ab8ab5a50ed6322cb846
Kevin Halliwell <khalliwe@asu.edu>
2019-10-24 16:27:28 -0400

    updates to weather app

--- weather.js
+++ weather.js
@@ -1,3 +1,8 @@
+var lat;
+var long;
+var locale;
+var apiCall;
+
 function weather(){
 	//Create variables
 	var location = document.getElementById("location");
@@ -5,86 +10,26 @@ function weather(){
 	var url = "https://api.darksky.net/forecast/";
 	var timezone = "America/New York";
 	getWeather();
+    apiCall = url + apiKey + locale;
+    $.getJSON(apiCall, function(forecast) {
+    console.log(forecast);
+});
+
 }
 
-var getWeather = function() {
+function getWeather() {
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(function(position){
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
         //showWeather(lat, long)
         console.log(lat);
         console.log(long);
+        locale = "[" + lat + "]" + "," + "[" + long + "]";
+        console.log(locale);
         
       })
     } else {
             window.alert("Current location could not be determined.");
       }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-/*//Create variables
-var location = document.getElementById("location");
-var apiKey = config;
-var url = "https://api.darksky.net/forecast/";
-var timezone = "America/New York";
-
-var geolocation{};
-if (navigator.geolocation){
-	navogator.geolocation.getCurrentPosition(success,error):
-} else {
-	alert("Your browser does not support Geolocation!!");
-}
-
-function error() {
- 	location.innerHTML = "Unable to retrieve your location";
-}
-
-function success(position) {
-	geolocation.latitude = position.coords.latitude;
-	geolocation.longitude = position.coords.longitude;
-
-	location.innerHTML =
-  		"Latitude is " + latitude + "° Longitude is " + longitude + "°";
-}*/
-
-/*function weather() {
-	var location = document.getElementById("location");
-  	var apiKey = config;
-  	var url = "https://api.darksky.net/forecast/";
-  	var timezone = "America/New York";
-
-  	navigator.geolocation.getCurrentPosition(success, error);
-
-  	function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-
-    location.innerHTML =
-      	"Latitude is " + latitude + "° Longitude is " + longitude + "°";
-
-    $.getJSON(
-      	url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
-      	function(data) {
-        	$(".temp").html(data.currently.temperature + "° F");
-       	 	$(".minutely").html(data.minutely.summary);
-      	}
-    );
-  	}
-
-  	function error() {
-    	location.innerHTML = "Unable to retrieve your location";
-  	}
-
-  	location.innerHTML = "Locating...";
-}*/
