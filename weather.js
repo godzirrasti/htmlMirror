30b09539793ad42fa0f8075fad0568cf7449f231
Kevin Halliwell <kevinhalliwell@me.com>
2020-02-25 10:33:54 -0500

    Weather logging

--- weather.js
+++ weather.js
@@ -1,13 +1,13 @@
 var apiCall;
-var lati = 35.0870163;
-var longi = -80.6881609;
-var apiKey = config.apiKey;
+var apiKey = apiConfig.apiKey;
+var lat = apiConfig.lat;
+var long = apiConfig.long;
 
 function weather(){
     var location = document.getElementById("location");
     var url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/";
     var timezone = "America/Charlotte";
-    apiCall = url + apiKey + lati + "," + longi;
+    apiCall = url + apiKey + lat + "," + long;
     $.getJSON(apiCall, function(forecast) {
         console.log(forecast);
     });
